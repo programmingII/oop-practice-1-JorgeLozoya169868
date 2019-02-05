292b24c0a98708aadace4f437356c155f3219e22
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-05 12:56:35 -0700

    Add files via upload

---
+++ Ejercicio3-Dividir_dos_numeros_imprimirlo_en_pantalla/Ejercicio3.java
@@ -0,0 +1,18 @@
+/*
+*@JorgeLozoyaAcosta 169868
+*@Feb/05/2019
+*@Start 12:49PM
+*
+*/
+/*
+*3. Write a Java program to divide two numbers and print on the screen.
+*/
+public class Ejercicio3{
+	public static void main(String[] args){
+	int x=86, y=2,z=x/y;
+	
+	System.out.println(x + " / " + y + " = " + z);
+
+	}
+
+}
