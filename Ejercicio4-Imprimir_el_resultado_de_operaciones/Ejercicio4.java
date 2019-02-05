ed22731c14ae10174e56483074a6b83459808a26
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-05 13:06:07 -0700

    Add files via upload

---
+++ Ejercicio4-Imprimir_el_resultado_de_operaciones/Ejercicio4.java
@@ -0,0 +1,20 @@
+/*
+*@author JorgeLozoyaAcosta 169868
+*@date Feb/05/2019
+*@Start 12:58PM
+*/
+/* 4. Write a Java program to print the result of the following operations.
+Test Data:
+a. -5 + 8 * 6
+b. (55+9) % 9 
+c. 20 + -3*5 / 8 
+d. 5 + 15 / 3 * 2 - 8 % 3 
+*/
+
+public class Ejercicio4{
+	public static void main(String[] args){
+	int a=-5 + 8 * 6, b = (55+9) % 9, c= 20+ -3*5/8, d= 5 + 15/3 * 2 -8 % 3;
+
+	System.out.println("a = "+a+"\n b = "+b+"\n c = "+c+"\n d = "+d);
+	}
+}
