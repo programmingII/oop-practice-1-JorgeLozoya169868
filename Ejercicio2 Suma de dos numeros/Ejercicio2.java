2dfa6530591dbdc0413b046f1d384e57d8e01cb5
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-05 12:48:25 -0700

    Add files via upload

---
+++ Ejercicio2 Suma de dos numeros/Ejercicio2.java
@@ -0,0 +1,17 @@
+/*
+*@JorgeLozoyaAcosta 169868
+*@Feb/05/2019
+*@Start 12:36PM
+*
+*/
+/*
+*2. Write a Java program to print the sum of two numbers.
+*/
+public class Ejercicio2{
+	public static void main(String[] args){
+	int z=15+33;
+	System.out.println("La suma de 15 + 33 = "+z);
+
+	}
+
+}
