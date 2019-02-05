43c2f90e4d00e1cc75a0403b96115aec930dd5c0
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-05 12:43:51 -0700

    Add files via upload

---
+++ Ejercicio1 Hello/Ejercicio1.java
@@ -0,0 +1,12 @@
+/*
+* @Jorge Lozoya
+* @Timestart 12:25 PM 
+* @ 2/feb/ 2019
+*/
+public class Ejercicio1{
+ 
+ public static void main(String[] args) {
+  System.out.println("Hello\nMy name is Jorge Lozoya!");
+ }
+ 
+}
