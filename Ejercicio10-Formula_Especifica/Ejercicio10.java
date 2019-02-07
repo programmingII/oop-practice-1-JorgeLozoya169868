e0d40482e127d2f006ae75b67c1306a92105512a
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-07 09:52:08 -0700

    Add files via upload

---
+++ Ejercicio10-Formula_Especifica/Ejercicio10.java
@@ -0,0 +1,18 @@
+/*
+* @author JorgeLozoyaAcosta 169868	
+* @date Feb/07/2019
+* @Start 9:43AM
+*/
+/*
+*10. Write a Java program to 
+*compute a specified formula.
+Specified Formula : 
+4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+*/
+public class Ejercicio10{
+	public static void main(String[] args){
+	double result = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+	System.out.println("El resultado es: "+result);
+}
+
+}
