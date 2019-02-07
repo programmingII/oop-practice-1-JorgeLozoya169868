1fc6ad7b5f0d62fc6e865298fcf9791a97576e32
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-07 09:19:54 -0700

    Add files via upload

---
+++ Ejercicio7-Tabla_de_multiplicacion/Ejercicio7.java
@@ -0,0 +1,28 @@
+/*
+*@author JorgeLozoyaAcosta 169868
+*@date feb/07/2019
+*@start 9:
+*/
+/*
+* 7. Write a Java program that takes a number as 
+* input and prints its multiplication table upto 10.
+*/
+/*
+*@param num sera el valor dado por el usuario
+*
+*/
+import java.util.Scanner;
+public class Ejercicio7{
+	public static void main(String[] args){
+	Scanner in = new Scanner(System.in);
+
+	System.out.println("Favor de dar la tabla de multiplicar que desea");
+		int num = in.nextInt();System.out.println("\n");
+
+	for(int i=0;i<=10;i++)
+	{
+	System.out.println(num+" * "+i+" = "+(num*i));
+	}
+	
+	}
+}
