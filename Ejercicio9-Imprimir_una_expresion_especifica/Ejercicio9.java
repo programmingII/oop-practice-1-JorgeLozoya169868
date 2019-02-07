3ab8be3397b20f503c755c000ef120181fd37860
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-07 09:40:48 -0700

    Add files via upload

---
+++ Ejercicio9-Imprimir_una_expresion_especifica/Ejercicio9.java
@@ -0,0 +1,16 @@
+/*
+* @author Jorge Lozoya Acosta 169868	
+* @date Feb/07/2019
+* @Start 9:35AM
+*/
+/*
+*9.Write a Java program to compute the 
+*specified expressions and print the output
+Specified Expression : 
+(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)
+*/
+public class Ejercicio9{
+	public static void main(String[] args){
+	System.out.println("(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) = "+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+	}
+}
