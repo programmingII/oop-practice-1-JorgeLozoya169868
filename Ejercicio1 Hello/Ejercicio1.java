2d13a3451f28762a2cb4b82e3cdd8584fe9a3b31
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-07 12:17:05 -0700

    Update Ejercicio1.java

--- Ejercicio1 Hello/Ejercicio1.java
+++ Ejercicio1 Hello/Ejercicio1.java
@@ -6,7 +6,7 @@
 public class Ejercicio1{
  
  public static void main(String[] args) {
-  System.out.println("Hello\nMy name is Jorge Lozoya!");
+  System.out.println("Hello\nMy name is Jorge Lozoya!"); //System.out.println es la forma de imprimir en pantalla e imprime lo que está entrecomillado
  }
  
 }
