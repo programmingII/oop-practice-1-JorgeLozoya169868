a6e747e1801acd1d4ecd575bd84e721617846def
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-07 09:28:51 -0700

    Add files via upload

---
+++ Ejercicio8-Imprimir_un_patron/Ejercicio8.java
@@ -0,0 +1,17 @@
+/*
+*@author JorgeLozoyaAcosta 169868
+*@date feb/07/2019
+*@start 9:22
+*/
+/*
+* 8. Write a Java program to display the following pattern.
+*/
+
+public class Ejercicio8{
+	public static void main(String[] args){
+	System.out.println("   J    a  v     v  a");	
+	System.out.println("   J   a a  v   v  a a");	
+	System.out.println("J  J  aaaaa  v v  aaaaa");	
+	System.out.println(" JJ  a     a  v  a     a");	
+	}	
+}
