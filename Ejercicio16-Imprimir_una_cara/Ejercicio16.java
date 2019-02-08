f9de25518acec5852a13d15ded977b196ba6f5b4
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-08 13:31:55 -0700

    Add files via upload

---
+++ Ejercicio16-Imprimir_una_cara/Ejercicio16.java
@@ -0,0 +1,20 @@
+/*
+* Nombre: Jorge Lozoya Acosta 169868
+* Dia: Feb/08/2019
+* Hora 1:23 PM
+*/
+/*
+16. Write a Java program to print a face.
+*/
+
+public class Ejercicio16
+{
+	public static void main(String[] args)
+	{
+	System.out.println(" +^^^^^^+"); //Se imprimira el patron de una cara, para ello se necesitaran 5 sout
+	System.out.println(" | ¬  ¬ |");
+	System.out.println("[|   >  |]");
+	System.out.println(" | ____ |");
+	System.out.println(" |^^^^^^|");		
+	}
+}
