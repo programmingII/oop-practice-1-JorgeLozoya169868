a3d00b74eae9223ae147b1d335920883a23b56c7
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-08 13:08:59 -0700

    Add files via upload

---
+++ Ejercicio14-U.S_Flag/Ejercicio14.java
@@ -0,0 +1,30 @@
+/*
+* Nombre: Jorge Lozoya Acosta 169868
+* Dia: Feb/08/2019
+* Hora 1:03 PM
+*/
+/*
+14. Write a Java program to print an American flag on the screen.
+*/
+
+public class Ejercicio14{
+	public static void main(String[] args)
+	{
+		System.out.println("* * * * * * =================================="); //Imprimir 15 println para lograr la bandera de U.S.
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * =================================="); 
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * =================================="); 
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * =================================="); 
+		System.out.println(" * * * * *  =================================="); 
+		System.out.println("* * * * * * =================================="); 
+		System.out.println("=============================================="); 
+		System.out.println("=============================================="); 
+		System.out.println("=============================================="); 
+		System.out.println("=============================================="); 
+		System.out.println("=============================================="); 
+		System.out.println("==============================================");
+ 
+	}
+}
