41ddfaf419ab362a03a2be1757367c20acc2df2a
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-25 08:42:51 -0700

    Add files via upload

---
+++ Ejercicio43-Twinkle_Twinkle/Ejercicio43.java
@@ -0,0 +1,21 @@
+/*
+* Nombre: Jorge Lozoya Acosta
+* Dia: 25/02/2019 
+* Hora: 8:43 AM
+*/
+/*
+43. Write a Java program to print the following
+string in a specific format (see the output).
+Output: 
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+*/
+class Ejercicio43{
+	public static void main(String[] args){
+	System.out.print("Twinkle, twinkle, little star, \n\t How I wonder what you are! \n\t\t Up above the world so high, \n\t\t Like a diamond in the sky. \n Twinkle, twinkle, little star, \n\t How I wonder what you are!  ");// \t representa un tab
+}
+}
