b08092e88242f7c94b0a0c1979eb9a886f46eada
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-02-25 18:45:11 -0700

    Add files via upload

---
+++ Ejercicio48-Numeros_impares/Ejercicio48.java
@@ -0,0 +1,16 @@
+/*
+* Nombre: Jorge Lozoya Acosta
+* Dia: 25/02/2019
+* Hora: 6:35 AM
+*/
+/*
+48. Write a Java program to print the odd numbers from 1 to 99. Prints one number per line.
+ */
+class Ejercicio48{
+	public static void main(String[] args){
+	for(int i = 1; i<=99;i +=2){
+		System.out.println("\t"+i); // Se imprimirá desde el 1 hasta el 99 de dos en dos, es decir 1,3,5,7,...,99
+	}
+
+}
+}
